daf3c5bb4775df1e27a290caa0ef1d12b2e60c49
lokreylatika <lokrey.latika@gmail.com>
2026-09-14 12:44:18 +0000

    Week 3: Added command parser using strtok

--- Makefile
+++ Makefile
@@ -4,7 +4,8 @@ CFLAGS = -Wall -Wextra -g -Iinclude
 
 SRC = src/main.c \
       src/input.c \
-      src/diagnostics.c
+      src/diagnostics.c \
+      src/parser.c
 
 TARGET = bin/diagnostics
 
--- src/main.c
+++ src/main.c
@@ -3,44 +3,51 @@
 #include <string.h>
 
 #include "../include/input.h"
+#include "../include/parser.h"
 #include "../include/diagnostics.h"
 
 int main()
 {
     char *line;
+    char **tokens;
 
     printf("===== Linux System Diagnostics Toolkit =====\n");
 
     while(1)
     {
-        printf("\n1. Show System Uptime\n");
-        printf("2. Show RAM Information\n");
-        printf("3. Exit\n");
-        printf("Enter your choice: ");
+        printf("diagnostics> ");
 
         line = read_line();
 
-        if(strcmp(line, "1") == 0)
-        {
-            showUptime();
-        }
-        else if(strcmp(line, "2") == 0)
-        {
-            showMemory();
-        }
-        else if(strcmp(line, "3") == 0)
+        if(strcmp(line, "exit") == 0)
         {
             free(line);
-            printf("Exiting Toolkit...\n");
             break;
         }
-        else
+
+        tokens = parse_line(line);
+
+        if(tokens[0] != NULL)
         {
-            printf("Invalid Choice\n");
+            if(strcmp(tokens[0], "uptime") == 0)
+            {
+                showUptime();
+            }
+            else if(strcmp(tokens[0], "memory") == 0)
+            {
+                showMemory();
+            }
+            else
+            {
+                printf("Unknown Command\n");
+            }
         }
 
+        free_tokens(tokens);
         free(line);
     }
+
+    printf("Goodbye!\n");
 
     return 0;
 }
